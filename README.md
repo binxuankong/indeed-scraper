f68d7c5901e08cb5dcbc35a4337822b26846d4b3
Bin Xuan Kong <binxuankong@gmail.com>
2021-03-22 14:46:27 +0800

    Cookie to prevent captcha

--- README.md
+++ README.md
@@ -69,9 +69,13 @@ python3 cleaner.py
 python3 cleaner_usa.py
 ```
 
-### Note
+### Circumevene Indeed IP block
 Running all the scripts concurrently might result in IP being blocked by Indeed. A shadow user agent and proxy rotation is being used in the script but they do not work very well. To circumstance this, try scraping at diffrent time and scrape for each job separately (Running the scraper script separtely at different time instead of using parallel).
 
+Another option is to visit any Indeed job postings website on a Chrome-based browser and Inspect > Network. Copy the **cookie** from the Request Headers from the browser and replace the cookie in the `configs.py` file.
+
+
+### Note
 Delete the old csv files every week before scraping for the new ones to prevent duplicated old job postings being uploaded to the database.
 
 The details required to connect to the database is not included in the code here for privacy purpose. Please contact me for the database connection information.
--- parallel/configs.py
+++ parallel/configs.py
@@ -1,3 +1,5 @@
+cookie = 'CTK=1evoebk1vocir800; INDEED_CSRF_TOKEN=I8cmiTxq0wBA6omNpPfxsBGUxECmMioi; LC="co=MY&hl=en_MY"; pjps=1; SHARED_INDEED_CSRF_TOKEN=I8cmiTxq0wBA6omNpPfxsBGUxECmMioi; SURF=yj1mYQPVsD5MSpSDWZOTcc0aqt7GknUE; __ssid=80a73f8cf819a5931cc38eab76d657d; PPID=eyJraWQiOiJkYTliOWYxNC01MzUwLTQxMWYtOGEyNC03NDMzODQxYTFhNTkiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJzdWIiOiI5NzNjMjkwZDEwNzc3MzM4IiwiYXVkIjoiYzFhYjhmMDRmIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF1dGgiOiJnb29nbGUiLCJjcmVhdGVkIjoxNDg5ODUyMDMyMDAwLCJyZW1fbWUiOnRydWUsImlzcyI6Imh0dHBzOlwvXC9zZWN1cmUuaW5kZWVkLmNvbSIsImV4cCI6MTYxNTUzNzY3NSwiaWF0IjoxNjE1NTM1ODc1LCJsb2dfdHMiOjE2MTU1MzU4NzUxODIsImVtYWlsIjoiYmlueHVhbmtvbmdAZ21haWwuY29tIn0.2_cDTgxK5LR77ogqZz5Usdzm7aW3hqlTpu5vaDExSsOGe3iWrpGGUC9TPg1qBVtqH156jaBTsKtTcmYRz09GMQ; SOCK="3WvAdtrWJtJKZFVwKYue7QnJZjY="; SHOE="w3mLTm42AhDt6ORsPUZacT1SiXefY4o5yCBp3_tp6LuSZxCzHRLv0BE8vb4NRAs12Rnk5hmrYKepwaMYX_790SoGV8SRbdjcGd59FHDeI6OdPjJO1ZwStEiqUCkeU5-_kyoEvHTnF7LKi49gfwEJ_NA="; CSRF=Cv9dg05IuhGqgbYEIKf73rMyFxJAlJtW; CO=US; temp_locale=en_US; RF="TFTzyBUJoNr6YttPP3kyivpZ6-9J49o-zoKsJMXlgx22td9Duad0ZJX8-Bwq8nJnVQziKe_qeEI="; LOCALE=en_US; indeed_rcc="LOCALE:PREF:LV:CTK:CO:UD:RQ"; LV="LA=1615535416:CV=1615533598:TS=1614654328"; UD="LA=1615535416:CV=1615533598:TS=1614654328"; IRF=sy4S5JpMGaOhLVrU5bWWLJFtme22RKODkbxvZsI25plwAoaIAXmpKw==; PCA=324e737fe0702ba0; ADOC=3106335063484798; INDEEDADS_HOME=426fb542451545bc|draw; ADV=1; INDEEDADS_PROMO2="pt=us-freetopost:pv=1c01d94ec5b1da3e:SG=308dfddc224b4045612c824fa842049b"; MICRO_CONTENT_CSRF_TOKEN=Yv8OQaGeCtXfQSFHgtY4FVH3jOnKsh7H; s_seven=TqwM1smcIP10FmmbLJMYWqPx215tc0L1cfKv99TB_vA; CO=US; LOCALE=en_US; PREF="TM=1616393970761:L=NY"; RQ="q=Machine_Learning_Engineer&l=NY&ts=1616394145835:q=Data+Analyst&l=NY&ts=1616393970812:q=Data+Analyst&l=LA&ts=1616392003115:q=Data+Analyst&l=WY&ts=1616391984476:q=Data+Scientist&l=MS&ts=1615774731459:q=Data+Scientist&l=NY&ts=1615774556621&pts=1615534874243:q=Data+Scientist&l=AZ&ts=1615535416791:q=Data+Scientist&l=AR&ts=1615534964825"; jaSerpCount=10; JSESSIONID=14AA07AC4BD0012F9071B4F8B6C0DA13'
+
 headers_list = [
     # Firefox 77 Mac
      {
@@ -7,7 +9,8 @@ headers_list = [
         "Referer": "https://www.google.com/",
         "DNT": "1",
         "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1"
+        "Upgrade-Insecure-Requests": "1",
+        "Cookie": cookie,
     },
     # Firefox 77 Windows
     {
@@ -18,21 +21,23 @@ headers_list = [
         "Referer": "https://www.google.com/",
         "DNT": "1",
         "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1"
+        "Upgrade-Insecure-Requests": "1",
+        "Cookie": cookie,
     },
     # Chrome 83 Mac
     {
-        "Connection": "keep-alive",
-        "DNT": "1",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "Cache-Control": "max-age=0",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
         "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Dest": "document",
         "Referer": "https://www.google.com/",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+        "Cookie": cookie,
     },
     # Chrome 83 Windows
     {
@@ -46,6 +51,7 @@ headers_list = [
         "Sec-Fetch-Dest": "document",
         "Referer": "https://www.google.com/",
         "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.9"
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cookie": cookie,
     },
 ]
