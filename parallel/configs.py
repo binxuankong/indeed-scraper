0f3e423880831ee55f658fa0c72f22e003035e23
Bin Xuan Kong <binxuankong@gmail.com>
2022-05-26 17:49:54 +0800

    Update configs cookie variable anme

--- parallel/configs.py
+++ parallel/configs.py
@@ -1,4 +1,4 @@
-cookie = 'CTK=1evoebk1vocir800; INDEED_CSRF_TOKEN=I8cmiTxq0wBA6omNpPfxsBGUxECmMioi; pjps=1; SHARED_INDEED_CSRF_TOKEN=I8cmiTxq0wBA6omNpPfxsBGUxECmMioi; SURF=yj1mYQPVsD5MSpSDWZOTcc0aqt7GknUE; CSRF=Cv9dg05IuhGqgbYEIKf73rMyFxJAlJtW; INDEEDADS_HOME=426fb542451545bc|draw; MICRO_CONTENT_CSRF_TOKEN=Yv8OQaGeCtXfQSFHgtY4FVH3jOnKsh7H; cmpsmeta="eNoBOADH/0pg5seiw/X+deMbWwcRQd6WQTfq7Xy9/28kV8EwJQaZysxbvM09xxU0mywpFVYL9p+RRAM1FBEFO0YZag=="; indeed_rcc="LOCALE:PREF:cmppmeta:LV:CTK:CO:UD:RQ"; PPID=""; LOCALE=en; RF=wKGgxUwMHWV1aKM-FTjqiMrjRY8zAroFxocW7SGr9uJASaKa98Jda29TEymDVLcsmEvuK92ca2Dtd5yKNhNcUBgdmE32QqAsj4kYiKmSXtfkS59j4IN-UAM1DzmHSUV4aRnkJVI_5G4RGh9-Lzs_m01rN0T-xzyQ; CMP_VISITED=1; LC="co=MY"; ROJC=dd8e28f89fd7db02; g_state={"i_p":1655109428991,"i_l":4}; LV="LA=1653300337:LV=1653277366:CV=1653300300:TS=1635136087"; PREF="TM=1653538225023:L=CA"; UD="LA=1653538225:CV=1653538225:TS=1653538225:SG=1e0b1a1dd591111c71d994524b4bbab8"; RQ="q=%22Data+Analyst%22&l=CA&ts=1653538225063:q=%22Machine+Learning+Engineer%22&l=AZ&ts=1653300337144&pts=1652690223621:q=%22Data+Analyst%22&l=AR&ts=1653277366887:q=%22Data+Engineer%22&l=HI&ts=1652077533496:q=%22Data+Analyst%22&l=TX&ts=1652072818855:q=%22Data+Scientist%22&l=NJ&ts=1652067104125:q=%22Data+Scientist%22&l=HI&ts=1652066124468:q=%22Data+Scientist%22&l=AL&ts=1650882762462:q=%22Data+Analyst%22&l=IL&ts=1650873325625:q=%22Data+Engineer%22&l=OR&ts=1650866403428"; jaSerpCount=343; gonetap=1; OptanonConsent=isGpcEnabled=1&datestamp=Thu+May+26+2022+12%3A10%3A27+GMT%2B0800+(Malaysia+Time)&version=6.30.0&isIABGlobal=false&hosts=&consentId=ad840c8c-6dca-4cb4-ba21-3698bc19b84b&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A0%2CC0003%3A0%2CC0004%3A0%2CC0007%3A0&AwaitingReconsent=false; ac=xjay4NypEeyCpElubWh0PQ#xjd2MNypEeyCpElubWh0PQ; CO=US; JSESSIONID=5FBC8D33C5DD2E57BFF913710708ABFB'
+custom_cookie = 'CTK=1evoebk1vocir800; INDEED_CSRF_TOKEN=I8cmiTxq0wBA6omNpPfxsBGUxECmMioi; pjps=1; SHARED_INDEED_CSRF_TOKEN=I8cmiTxq0wBA6omNpPfxsBGUxECmMioi; SURF=yj1mYQPVsD5MSpSDWZOTcc0aqt7GknUE; CSRF=Cv9dg05IuhGqgbYEIKf73rMyFxJAlJtW; INDEEDADS_HOME=426fb542451545bc|draw; MICRO_CONTENT_CSRF_TOKEN=Yv8OQaGeCtXfQSFHgtY4FVH3jOnKsh7H; cmpsmeta="eNoBOADH/0pg5seiw/X+deMbWwcRQd6WQTfq7Xy9/28kV8EwJQaZysxbvM09xxU0mywpFVYL9p+RRAM1FBEFO0YZag=="; indeed_rcc="LOCALE:PREF:cmppmeta:LV:CTK:CO:UD:RQ"; PPID=""; LOCALE=en; RF=wKGgxUwMHWV1aKM-FTjqiMrjRY8zAroFxocW7SGr9uJASaKa98Jda29TEymDVLcsmEvuK92ca2Dtd5yKNhNcUBgdmE32QqAsj4kYiKmSXtfkS59j4IN-UAM1DzmHSUV4aRnkJVI_5G4RGh9-Lzs_m01rN0T-xzyQ; CMP_VISITED=1; LC="co=MY"; ROJC=dd8e28f89fd7db02; g_state={"i_p":1655109428991,"i_l":4}; LV="LA=1653300337:LV=1653277366:CV=1653300300:TS=1635136087"; PREF="TM=1653538225023:L=CA"; UD="LA=1653538225:CV=1653538225:TS=1653538225:SG=1e0b1a1dd591111c71d994524b4bbab8"; RQ="q=%22Data+Analyst%22&l=CA&ts=1653538225063:q=%22Machine+Learning+Engineer%22&l=AZ&ts=1653300337144&pts=1652690223621:q=%22Data+Analyst%22&l=AR&ts=1653277366887:q=%22Data+Engineer%22&l=HI&ts=1652077533496:q=%22Data+Analyst%22&l=TX&ts=1652072818855:q=%22Data+Scientist%22&l=NJ&ts=1652067104125:q=%22Data+Scientist%22&l=HI&ts=1652066124468:q=%22Data+Scientist%22&l=AL&ts=1650882762462:q=%22Data+Analyst%22&l=IL&ts=1650873325625:q=%22Data+Engineer%22&l=OR&ts=1650866403428"; jaSerpCount=343; gonetap=1; OptanonConsent=isGpcEnabled=1&datestamp=Thu+May+26+2022+12%3A10%3A27+GMT%2B0800+(Malaysia+Time)&version=6.30.0&isIABGlobal=false&hosts=&consentId=ad840c8c-6dca-4cb4-ba21-3698bc19b84b&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A0%2CC0003%3A0%2CC0004%3A0%2CC0007%3A0&AwaitingReconsent=false; ac=xjay4NypEeyCpElubWh0PQ#xjd2MNypEeyCpElubWh0PQ; CO=US; JSESSIONID=5FBC8D33C5DD2E57BFF913710708ABFB'
 
 headers_list = [
     # Firefox 77 Mac
@@ -10,7 +10,6 @@ headers_list = [
         "DNT": "1",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
-        "Cookie": cookie,
     },
     # Firefox 77 Windows
     {
@@ -22,7 +21,6 @@ headers_list = [
         "DNT": "1",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
-        "Cookie": cookie,
     },
     # Chrome 83 Mac
     {
@@ -37,7 +35,6 @@ headers_list = [
         "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
         "Referer": "https://www.google.com/",
-        "Cookie": cookie,
     },
     # Chrome 83 Windows
     {
@@ -52,6 +49,5 @@ headers_list = [
         "Referer": "https://www.google.com/",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-US,en;q=0.9",
-        "Cookie": cookie,
     },
 ]
